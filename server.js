a78f493ce1002979d18583e2750a8bc3b9c30184
Tim-Smart <tim@fostle.com>
2010-08-29 05:04:34 +1200

    server.js init

---
+++ server.js
@@ -0,0 +1,11 @@
+var router = new (require('biggie-router'))();
+
+router.get('/').module('gzip').bind(function (request, response) {
+  response.sendBody('Index page.');
+});
+
+router.get(/^\/.*\.(js|css|html).*$/).module('gzip');
+
+router.module('static', __dirname + '/public').bind(function (request, response) {
+  response.sendBody(404, 'Asset not found: ' + request.uri);
+});
