db917e7f1f7c18f38981cce0277f77a76a616a76
Tim-Smart <tim@fostle.com>
2010-08-29 05:54:14 +1200

    Deploy

--- build/api.js
+++ build/api.js
@@ -1,7 +1,16 @@
-var sys;
+var respondWith404, sys;
 sys = require('sys');
 module.exports = function() {
-  return function(request, response, next) {
-    return response.sendBody(sys.inspect(arguments));
+  return function(request, response, next, path) {
+    return response.sendJson({
+      result: {
+        path: path
+      }
+    });
   };
+};
+respondWith404 = function(request, response) {
+  return response.sendBody(404, {
+    error: "Resource not found"
+  });
 };
