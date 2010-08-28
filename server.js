6e8e7beabdf5c5f06eabb1487911e330b58efff5
Tim-Smart <tim@fostle.com>
2010-08-29 05:09:22 +1200

    Forgot to .listen

--- server.js
+++ server.js
@@ -9,3 +9,5 @@ router.get(/^\/.*\.(js|css|html).*$/).module('gzip');
 router.module('static', __dirname + '/public').bind(function (request, response) {
   response.sendBody(404, 'Asset not found: ' + request.uri);
 });
+
+router.listen(80);
